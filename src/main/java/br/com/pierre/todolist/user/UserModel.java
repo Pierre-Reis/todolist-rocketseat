a59b0a8d371750e79e3d4ed7a18a0da38c20b823
Pierre Reis <pierre.developer@hotmail.com>
2024-07-09 21:28:23 -0300

    Projeto rocketseat todolist

---
+++ src/main/java/br/com/pierre/todolist/user/UserModel.java
@@ -0,0 +1,32 @@
+package br.com.pierre.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="tb_users")
+public class UserModel {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private UUID id;
+    
+    @Column(unique=true)
+    private String username;
+    
+    private String name;
+    
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+}
